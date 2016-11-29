ee7073d6d53b7fc0b318b0bb161c6b88eb27e91d
Tamás Benke <tombenke@gmail.com>
2016-11-29 21:02:35 +0100

    Add config data Write initial version of the system_init.sh script for the hedgehog-garden system

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 giri-configs
 ============
 
+Central repository for the different kind of giri configurations.
+
+This repository holds the configuration files in JSON (or YAML) format.
+Each configuration has its own subfolder under the `systems` directory.
+
+The `bin` folder contains the utilities, which makes possible the setup of the several systems based on the config files.
+
+## Usage
+
+In order to initialize a system you have to execute a two-step process:
+
+1. Set the required environment.
+
+   Each system configuration contains at least one `env` file, that you can source.
+   Choose that one which most adequate to your needs in case of that specific system.
+
+   For example:
+
+        . systems/hedgehog-garden/local.env
+
+2. Configure the system, using the `system_init.sh` script and the config data files placed into its own folder of each system.
+
+   For example:
+
+        systems/hedgehog-garden/system_init.sh
+
 ## References
 
 - [giri home page](https://github.com/tombenke/giri)
--- systems/hedgehog-garden/system_init.sh
+++ systems/hedgehog-garden/system_init.sh
@@ -7,32 +7,30 @@
 
 declare -rx SCRIPT_DESCRIPTION="giri system initialization"
 declare -rx RUNDIR=`dirname ${0}`
-declare -rx FIXTURES="../lib/fixtures/"
-declare -rx HOST=localhost
-declare -rx PORT=3000
-declare -rx URI_PREFIX="giri"
-declare -rx SERVER="http://${HOST}:${PORT}/${URI_PREFIX}"
-declare -rx JSONDIR="${RUNDIR}/${FIXTURES}"
 
 # Either source the required environment variables from the `local.env` file
 # or set it on other way in advance, before starting the script.
-#. ${RUNDIR}/local.env
+. ${RUNDIR}/local.env
+
+declare -rx SERVER="http://${HOST}:${PORT}/${URI_PREFIX}"
+declare -rx JSONDIR="${RUNDIR}"
 
 # Echo some hints on what is happening
 echo "Running ${SCRIPT_DESCRIPTION} script in ${RUNDIR}"
 echo "Use '${JSONDIR}' as base folder for data JSON config files"
 
-# Create system
+echo "Create system..."
 curl -X PUT ${SERVER}/systems/2a1152ee-4d77-4ff4-a811-598555937625 \
     -H "Content-Type: application/json" \
-    --data-binary @${JSONDIR}/updateSystemReq.json
+    --data-binary @${JSONDIR}/system_hedgehog-garden.json
 
-# Create Clusters
-curl -X PUT ${SERVER}/cluesters/2a1152ee-4d77-4ff4-a811-598555937625 \
-    -H "Content-Type: application/json" \
-    --data-binary @${JSONDIR}/updateClusterReq.json
+echo
+#echo "Create Clusters..."
+#curl -X PUT ${SERVER}/cluesters/2a1152ee-4d77-4ff4-a811-598555937625 \
+#    -H "Content-Type: application/json" \
+#    --data-binary @${JSONDIR}/cluster_open-air.json
 
-
-# See the results
+echo
+echo "See the systems created:"
 curl ${SERVER}/systems
-
+echo
